350b22bb5befcfde564931feb1bf700f5364eb05
nkwaaaa <nkwaaaa@gmail.com>
2023-05-30 19:54:26 -0300

    add create task

--- src/app/new/page.js
+++ src/app/new/page.js
@@ -1,3 +1,35 @@
-export default function page() {
-  return <div>task form</div>;
+"use client";
+
+import { useTasks } from "@/context/TasksContext";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function Page() {
+  const [task, setTask] = useState("");
+
+  const { createTask } = useTasks();
+  const router = useRouter();
+
+  const handleChange = (e) => {
+    setTask({ ...task, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(task);
+    createTask(task.title, task.description);
+    router.push("/");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input onChange={handleChange} name="title" placeholder="write a title" />
+      <textarea
+        onChange={handleChange}
+        name="description"
+        placeholder="write a description"
+      />
+      <button>save</button>
+    </form>
+  );
 }
--- src/context/TasksContext.js
+++ src/context/TasksContext.js
@@ -1,6 +1,7 @@
 "use client";
 
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { v4 as uuid } from "uuid";
 
 export const TaskContext = createContext();
 
@@ -11,7 +12,7 @@ export const useTasks = () => {
 };
 
 export const TaskProvider = ({ children }) => {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     {
       id: 1,
       title: "my first task",
@@ -27,9 +28,22 @@ export const TaskProvider = ({ children }) => {
       title: "my third task",
       description: "some third description",
     },
-  ];
+  ]);
+
+  const createTask = (title, description) => {
+    setTasks([
+      ...tasks,
+      {
+        title,
+        description,
+        id: uuid(),
+      },
+    ]);
+  };
 
   return (
-    <TaskContext.Provider value={{ tasks }}>{children}</TaskContext.Provider>
+    <TaskContext.Provider value={{ tasks, createTask }}>
+      {children}
+    </TaskContext.Provider>
   );
 };
