877451d496f8ae77d27d755a594d1c3124309cdf
nkwaaaa <nkwaaaa@gmail.com>
2023-05-30 19:54:41 -0300

    add navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { TaskProvider } from "@/context/TasksContext";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <TaskProvider>{children}</TaskProvider>
+        <TaskProvider>
+          <Navbar />
+
+          {children}
+        </TaskProvider>
       </body>
     </html>
   );
